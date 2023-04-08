c08ca91dac0b6e7cf8d8f3c75e7026a2ed2a88b9
kumpon0914 <61109015+kumpon0914@users.noreply.github.com>
2023-04-08 13:27:11 +0700

    .commits

---
+++ setup.sh
@@ -0,0 +1,15 @@
+
+Kairung
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"kumponpa@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
