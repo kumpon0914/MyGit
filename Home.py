4066be5afb2379b7a66a6766bddec810924e2726
kumpon0914 <61109015+kumpon0914@users.noreply.github.com>
2023-04-08 13:52:04 +0700

    .commit

--- Home.py
+++ Home.py
@@ -7,4 +7,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 st.header('Kumpon')
-st.image('./Pic/Cat.jpg')
+st.image('./Pic/BackG.jpg')
+html_8 = """
+<div style="background-color:#34495E;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
