4ba6544dbca95621be7409472028e942f3668d9f
kumpon0914 <61109015+kumpon0914@users.noreply.github.com>
2023-04-08 13:54:32 +0700

    .commit

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 st.header('Kumpon')
 st.image('./Pic/BackG.jpg')
 html_8 = """
-<div style="background-color:#34495E;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#B2BABB;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
 </div>
 """
